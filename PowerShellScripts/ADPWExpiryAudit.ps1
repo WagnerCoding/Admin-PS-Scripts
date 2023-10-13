77c379a036fe0c1156c947935647b14be42828ce
WagnerCoding <102889180+WagnerCoding@users.noreply.github.com>
2023-10-13 17:56:36 -0600

    Update ADPWExpiryAudit.ps1

--- PowerShellScripts/ADPWExpiryAudit.ps1
+++ PowerShellScripts/ADPWExpiryAudit.ps1
@@ -1,2 +1,5 @@
-﻿$Date = Get-Date -UFormat "%a, %b %d, %Y Time %H.%M.%S"
-Get-ADUser -filter { Enabled -eq $True -and PasswordNeverExpires -eq $False } -Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "Displayname", @{Name = "ExpiryDate"; Expression = { [datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed") } } | Sort-Object -property ExpiryDate | Export-Excel -Path "<desired path for output file>\ADPasswordExpiryAudit.xlsx" -AutoSize -TableName ADPasswordExpiryAudit -WorksheetName "$Date" ; Import-Excel -Path "<path to output file>\ADPasswordExpiryAudit.xlsx" | Out-Gridview -wait
+Install-Module -Name ImportExcel -RequiredVersion 5.4.2
+ 
+$Date = Get-Date -UFormat "%a, %b %d, %Y Time %H.%M.%S"
+mkdir C:\Audits
+Get-ADUser -filter { Enabled -eq $True -and PasswordNeverExpires -eq $False } -Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "Displayname", @{Name = "ExpiryDate"; Expression = { [datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed") } } | Sort-Object -property ExpiryDate | Export-Excel -Path "C:\Audits\ADPasswordExpiryAudit.xlsx" -AutoSize -TableName ADPasswordExpiryAudit -WorksheetName "$Date" ; Import-Excel -Path "C:\Audits\ADPasswordExpiryAudit.xlsx" | Out-Gridview -wait
