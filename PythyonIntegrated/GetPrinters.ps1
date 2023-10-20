4268a906034defe3c7eca56d43f8cc4ec4630556
WagnerCoding <102889180+WagnerCoding@users.noreply.github.com>
2023-10-20 10:27:58 -0600

    Update GetPrinters.ps1

--- PythyonIntegrated/GetPrinters.ps1
+++ PythyonIntegrated/GetPrinters.ps1
@@ -1,4 +1,4 @@
      Install-Module ImportExcel -Force
      Import-Module ImportExcel
      $Date = Get-Date -UFormat "%a, %b %d, %Y Time %H.%M.%S"
-     Get-Printer | Select-Object -Property Name, PrinterStatus, DriverName, PortName | Export-Excel -Path "C:\$Env:Computername.xlsx" -AutoSize -TableName "$Env:Computername_Printers" -WorksheetName "$Date"
+     Get-Printer | Select-Object -Property Name, PrinterStatus, DriverName, PortName | Export-Excel -Path "C:\Printers\$Env:Computername.xlsx" -AutoSize -TableName "$Env:Computername_Printers" -WorksheetName "$Date"
